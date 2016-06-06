d8ec975a39bb0cf96bbdd8a760f5efaa18331f80
李振宇 <lichenyu@lizhenyude-MacBook-Pro.local>
2016-06-06 19:00:28 +0800

    Hot Module Replacement

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './app.css';
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <h1>Hello, world!123123123123qweqweeeeeesssss</h1>,
   document.querySelector('#wrapper')
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,34 @@
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+var webpack = require('webpack');
+
 module.exports = {
-  entry: {
-    bundle: './main.js',
-    bundle2: './main2.js'
-  },
+  entry: [
+    'webpack/hot/dev-server',
+    'webpack-dev-server/client?http://localhost:8080',
+    './main.js'
+  ],
   output: {
-    filename: '[name].js'
+    filename: 'bundle.js',
+    publicPath: '/static/'
   },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
   module: {
     loaders: [
      {
        test: /\.jsx?$/,
        exclude: /node_modules/,
-       loader: 'babel',
-       query: {
-         presets: ['es2015', 'react']
-       }
+       loader: 'babel-loader?presets[]=es2015&presets[]=react',
      },
      {
        test: /\.css$/,
        loader: 'style-loader!css-loader'
+     },
+     {
+       test: /\.(png|jpg)$/,
+       loader: 'url-loader?limit=8192'
      },
    ]
  }
